216c75ce9784210df7c4758844b72fb7b819512d
shadhini <jshadhiniaseka@gmail.com>
2019-09-12 07:16:57 +0530

    add functions to create dataframe in different ways from a given python list of lists

--- panda_dataframes/__init__.py
+++ panda_dataframes/__init__.py
@@ -1,2 +1,6 @@
 from .timeseries import create_datetime_index
-from .create_dataframe import csv_to_df
+from .create_dataframe import csv_to_df, \
+    list_of_lists_to_df, \
+    list_of_lists_to_df_first_row_as_columns, \
+    list_of_lists_to_df_first_column_as_index, \
+    list_of_lists_to_df_first_row_as_columns_first_column_as_index
