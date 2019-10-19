771af428fa4fca799f367cdcfb46dba1f6752771
shadhini <jshadhiniaseka@gmail.com>
2019-10-19 15:55:59 +0530

    bash hello world

---
+++ bash_utils/hello_world/hello_world.sh
@@ -0,0 +1,58 @@
+############
+# SHE BANG
+############
+#** specify the path to the interpreter  (or program) that should be used to run (or interpret)
+#** the rest of the lines in the text file.
+#!/bin/bash
+
+##########
+# PRINTS
+##########
+#echo "Hello, World!"
+
+#############
+# VARIABLES
+#############
+#ABC="abc" #** make sure to not include spaces before or after the assignment (=) sign
+#directory=/etc
+#echo $ABC $directory
+
+#** Single quotes will treat every character literally.
+#** Double quotes will allow you to do substitution (that is include variables within the setting of the value)
+#myvar='Hello World'
+#echo $myvar
+#newvar="More $myvar"
+#echo $newvar
+#newvar='More $myvar'
+#echo $newvar
+
+
+##########################
+# Command Line Arguments
+##########################
+#cp $1 $2
+#echo Details for $2
+#ls -lh $2
+
+#########################
+# Special Variables
+#########################
+#$0 - The name of the Bash script.
+#$1 - $9 - The first 9 arguments to the Bash script. (As mentioned above.)
+#$# - How many arguments were passed to the Bash script.
+#$@ - All the arguments supplied to the Bash script.
+#$? - The exit status of the most recently run process.
+#$$ - The process ID of the current script.
+#$USER - The username of the user running the script.
+#$HOSTNAME - The hostname of the machine the script is running on.
+#$SECONDS - The number of seconds since the script was started.
+#$RANDOM - Returns a different random number each time is it referred to.
+#$LINENO - Returns the current line number in the Bash script.
+
+###########################
+# Command Substitution
+###########################
+#** take the output of a command or program (what would normally be printed to the screen) and save it as the value of a variable.
+#** command should be placed within brackets, preceded by a $ sign.
+myvar=$(date -r /home/shadhini/dev/repos/shadhini/python_helpers/bash_utils/hello_world/original.txt)
+echo $myvar
---
+++ multiprocessing/parallel_processing.py
@@ -0,0 +1,27 @@
+import multiprocessing as mp
+import traceback
+from datetime import datetime, timedelta
+
+
+def write_to_file(file_name, data, string):
+    print(string)
+    print(datetime.now())
+    with open(file_name, 'w+') as f:
+        f.write('\n'.join(data))
+        f.write(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+
+
+wrf_systems_list = "A,C,E,SE".split(',')
+
+try:
+    mp_pool = mp.Pool(mp.cpu_count())
+
+    wrf_results = mp_pool.starmap_async(extract_wrf_data,
+                                        [(wrf_system, config_data, tms_meta) for wrf_system in wrf_systems_list]).get()
+
+    print("wrf extraction results: ", wrf_results)
+except Exception as e:
+    msg = 'Multiprocessing error.'
+    traceback.print_exc()
+finally:
+    mp_pool.close()
