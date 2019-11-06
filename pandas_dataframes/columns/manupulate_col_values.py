07ea47b96524cc6a6e0da90f13abe7c615458ba7
shadhini <jshadhiniaseka@gmail.com>
2019-11-06 10:18:36 +0530

    select unique values of a column

---
+++ pandas_dataframes/columns/manupulate_col_values.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+
+coefficients = pd.read_csv('sb_rf_coefficients.csv', delimiter=',')
+
+# unique values of a column --> dataframe.coulumn_name.unique()
+distinct_ids = coefficients.curw_obs_id.unique()
+
+for i in distinct_ids:
+    print(i)
+
