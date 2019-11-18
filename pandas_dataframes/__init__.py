36555e887fd2b0aee1ece161f2df49f059df49c4
shadhini <jshadhiniaseka@gmail.com>
2019-11-19 00:38:27 +0530

    extract only hourly data from random timeseries

--- pandas_dataframes/__init__.py
+++ pandas_dataframes/__init__.py
@@ -1,7 +1,1 @@
-from .timeseries import create_empty_df_with_datetime_index
-from .create_dataframe import csv_to_df, \
-    list_of_lists_to_df, \
-    list_of_lists_to_df_first_row_as_columns, \
-    list_of_lists_to_df_first_column_as_index, \
-    list_of_lists_to_df_first_row_as_columns_first_column_as_index
 from .interpolations import ts_linear_interpolation
--- pandas_dataframes/timeseries/timeseries.py
+++ pandas_dataframes/timeseries/timeseries.py
@@ -1,6 +1,8 @@
 from datetime import datetime, timedelta
 import pandas as pd
 
+from pandas_dataframes.create_df import csv_to_df
+
 
 def create_empty_df_with_datetime_index(start, end, timestep):
     """
@@ -15,3 +17,12 @@ def create_empty_df_with_datetime_index(start, end, timestep):
 
 
 # print(create_datetime_index(start="2019-09-11 23:30:00", end="2019-09-12 23:30:00", timestep=120))
+
+
+### extract only hourly data from random timeseries ###
+original_ts = csv_to_df('naula_observed.csv', ',')
+original_ts['time'] = pd.to_datetime(original_ts['time'], format="%Y-%m-%d %H:%M:%S") # .astype('datetime64')
+df = (pd.date_range(start="2019-10-23 23:00:00", end="2019-10-25 10:00:00", freq='60min')).to_frame(name='time')
+hourly_ts = pd.merge(df, original_ts, on='time', how='left')
+print(hourly_ts)
+
