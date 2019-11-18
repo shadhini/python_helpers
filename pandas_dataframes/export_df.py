a371bee26c16bb0fe5346c55d5457cc816c91b56
shadhini <jshadhiniaseka@gmail.com>
2019-11-18 23:55:55 +0530

    update

--- pandas_dataframes/export_df.py
+++ pandas_dataframes/export_df.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 ###### save to file ###############
 df = pd.DataFrame()
-pd.to_csv("file_name.txt", columns=['value'], header=False, index=None)
+df.to_csv("file_name.txt", columns=['value'], header=True, index=True)
 
 ###### pandas df to list of lists #####
 df.reset_index().values.tolist()
