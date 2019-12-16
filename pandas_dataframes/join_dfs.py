d0ed660255b60deba8a6e6ad248cdd4d3451d502
shadhini <jshadhiniaseka@gmail.com>
2019-12-16 06:30:59 +0530

    update pandas

--- pandas_dataframes/join_dfs.py
+++ pandas_dataframes/join_dfs.py
@@ -2,9 +2,23 @@ import pandas as pd
 import numpy as np
 
 
-from pandas_dataframes import create_empty_df_with_datetime_index
-from pandas_dataframes import list_of_lists_to_df_first_column_as_index
-from pandas_dataframes import ts_linear_interpolation
+def create_empty_df_with_datetime_index(start, end, timestep):
+    index = pd.date_range(start=start, end=end, freq='{}min'.format(timestep))
+    return pd.DataFrame(index=index)
+
+
+def list_of_lists_to_df_first_column_as_index(data):
+    original_data = np.array(data)
+    index = original_data[:, 0]
+    data = original_data[:, 1:]
+
+    return pd.DataFrame.from_records(data=data, index=index)
+
+
+def ts_linear_interpolation(dataframe, interpolation_method):
+
+    new_dataframe = pd.to_numeric(dataframe[dataframe.columns[0]], errors='coerce')
+    return new_dataframe.interpolate(method=interpolation_method, limit_direction='both')
 
 
 TS2 = [["2019-08-22 01:00:00", 1.8], ["2019-08-22 02:30:00", 1.5], ["2019-08-22 03:30:00", 1.4],["2019-08-22 07:30:00", 2.4],
