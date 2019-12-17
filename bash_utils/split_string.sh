127b6e45b688332c9fd18061a2841e96e63448b3
shadhini <jshadhiniaseka@gmail.com>
2019-12-17 17:52:57 +0530

    bash substring and split string

---
+++ bash_utils/split_string.sh
@@ -0,0 +1,17 @@
+d='18'
+
+date=$1
+if [ -z $date ]
+then
+  date=$(date -u -d '-1 day' '+%F')
+fi
+
+IFS='-' read -ra DATELIST <<< "$date"
+echo $DATELIST
+session_id=""
+for i in "${DATELIST[@]}"; do
+    session_id="$session_id$i"
+done
+session_id="$session_id$d"
+echo $session_id
+
---
+++ bash_utils/substring.sh
@@ -0,0 +1,9 @@
+u="this is a test"
+var="${u:10:4}"
+echo "${var}"
+
+a=2019124589
+b=${a:0:8}
+echo $b
+
+#/home/shadhini/dev/repos/shadhini/python_helpers/bash_utils/substring.sh
