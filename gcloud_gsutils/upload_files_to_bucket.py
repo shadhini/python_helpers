b31aaf1481c8ab2ba9260883fa692248fd94b7de
shadhini <jshadhiniaseka@gmail.com>
2019-12-19 15:39:16 +0530

    gsutil command not found issue solved

--- gcloud_gsutils/upload_files_to_bucket.py
+++ gcloud_gsutils/upload_files_to_bucket.py
@@ -4,3 +4,8 @@ local_file_path = "/home/shadhini/dev/repos/shadhini/python_helpers/gcloud_gsuti
 bucket_output_dir = "wrf_nfs/wrf/temp/"
 
 os.system("gsutil cp {} gs://{}".format(local_file_path, bucket_output_dir))
+
+# OR (if the above command doesn't work and gives a "gsutil comman not found error",
+# give the absolute path to gsutil binaries in the command as follows)
+
+os.system("/home/muditha/google-cloud-sdk/bin/gsutil cp {} gs://{}".format(local_file_path, bucket_output_dir))
