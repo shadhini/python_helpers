4648d9d24f7a1fa9c55265e55558691a1444e162
shadhini <jshadhiniaseka@gmail.com>
2020-02-18 15:24:31 +0530

    retrieve distinct combination of a dataframe selected columns

--- pandas_dataframes/values/distinct_values.py
+++ pandas_dataframes/values/distinct_values.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import numpy as np
 
+#########################################
+# Distinct Values :: Single Column
+#########################################
 coefficients = pd.read_csv('sb_rf_coefficients.csv', delimiter=',')
 
 #### iterate through distict values
@@ -13,3 +16,14 @@ distinct_ids = coefficients.curw_obs_id.unique()
 
 for i in distinct_ids:
     print(i)
+
+#########################################
+# Distinct Combinations :: Multiple Column
+#########################################
+
+df1 = pd.DataFrame({'A':['yes','yes','yes','yes','no','no','yes','yes','yes','no'],
+                   'B':['yes','no','no','no','yes','yes','no','yes','yes','no']})
+
+df1.groupby(['A','B']).size()
+
+df1.groupby(['A','B']).size().reset_index().rename(columns={0:'count'})
