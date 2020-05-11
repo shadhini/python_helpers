588e6e0d901f8927f2d3391bf534d3b6869db872
shadhini <jshadhiniaseka@gmail.com>
2020-05-11 13:33:31 +0530

    add function to retrieve file last modified time in SL time irrespective of the system timezone

--- file_utils/extract_file_properties.py
+++ file_utils/extract_file_properties.py
@@ -3,7 +3,7 @@ import time
 from datetime import datetime, timedelta
 
 
-def get_file_last_modified_time(file_path):
+def get_file_last_modified_time_offset_5_30(file_path):
     # os.path.getmtime() returns modified time as seconds since the epoch
     # time.gmtime() converts seconds since the epoch	time.struct_time in UTC
     # time.localtime() converts seconds since the epoch	time.struct_time in localime
@@ -14,4 +14,17 @@ def get_file_last_modified_time(file_path):
     return time.strftime('%Y-%m-%d %H:%M:%S', modified_time)
 
 
-print(get_file_last_modified_time("/home/shadhini/dev/repos/shadhini/curw_helpers/netcdf_utils/d03_RAINNC_2019-05-06_A.csv"))
+def get_file_last_modified_time_SL(file_path):
+
+    modified_time_raw = os.path.getmtime(file_path)
+    print("modified_time_raw: ", modified_time_raw)
+
+    modified_time_utc = datetime.utcfromtimestamp(modified_time_raw)
+    print("modified_time_utc: ", modified_time_utc)
+
+    # # returns local time (UTC + 5 30)
+    modified_time_SL = modified_time_utc + timedelta(hours=5, minutes=30)
+
+    return modified_time_SL.strftime('%Y-%m-%d %H:%M:%S')
+
+print(get_file_last_modified_time_SL("/home/shadhini/dev/repos/shadhini/python_helpers/file_utils/test_file.txt"))
