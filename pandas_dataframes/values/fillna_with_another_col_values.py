70dc9855678e8d4e7ed3fc53a365e235f1160c69
shadhini <jshadhiniaseka@gmail.com>
2020-05-14 17:48:16 +0530

    function to fill NaN values with another column values

---
+++ pandas_dataframes/values/fillna_with_another_col_values.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import numpy as np
+
+# df = pd.DataFrame(
+#     np.array([[1, 2, 3], [4, 5, np.nan], [7, 8, 9], [3, 2, np.nan], [5, 6, np.nan]]),
+#     columns=['a', 'b', 'c']
+# )
+#
+# print(df)
+#
+# df['c'] = df.apply(
+#     lambda row: row['a']*row['b'] if np.isnan(row['c']) else row['c'],
+#     axis=1
+# )
+#
+# print(df)
+
+df = pd.DataFrame(
+    np.array([[1, 2, 3], [np.nan, 4, 6], [np.nan, 6, 9], [np.nan, np.nan, 12], [np.nan, np.nan, np.nan]]),
+    columns=['a', 'b', 'c']
+)
+
+print(df)
+
+# df['Cat1'].fillna(df['Cat2'])
+
+df['b'] = df['b'].fillna(df['c'])
+df['a'] = df['a'].fillna(df['b'])
+
+print(df)
