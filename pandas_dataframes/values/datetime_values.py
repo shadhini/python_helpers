81125c23f5700cc19d12e50d04b143b4db04ba62
shadhini <jshadhiniaseka@gmail.com>
2020-05-14 22:50:31 +0530

    convert datetime index to string type

--- pandas_dataframes/values/datetime_values.py
+++ pandas_dataframes/values/datetime_values.py
@@ -9,3 +9,9 @@ df['time'] = df['time'].dt.strftime('%Y-%m-%d')
 
 # convert string column to datetime type
 df['time'] = pd.to_datetime(df['time'], format=COMMON_DATE_TIME_FORMAT)
+
+# convert datetime index to string type
+df.set_index('time', inplace=True)
+final_df = df['final'].reset_index()
+final_df['time'] = final_df['time'].dt.strftime(COMMON_DATE_TIME_FORMAT)
+final_ts = final_df.values.tolist()
