24fe6b8bf0fc0a771f000930ecf7130e4a6a8826
shadhini <jshadhiniaseka@gmail.com>
2020-05-15 21:18:47 +0530

    match string patterns

---
+++ string_utils/match_string_patterns.py
@@ -0,0 +1,29 @@
+import re
+import sys
+
+# prog = re.compile('([A-Z]\d+)+')
+prog = re.compile('flo2d_10_+')
+
+# fbakjbg
+# not matched
+# svsjvsnkv153
+# not matched
+# SESGSG12
+# not matched
+# S2
+# matched
+# D22
+# matched
+# R222
+# matched
+# D2DDNYDMYYYYYM
+# matched
+
+while True:
+  line = sys.stdin.readline()
+  if not line: break
+
+  if prog.match(line):
+    print ('matched')
+  else:
+    print ('not matched')
