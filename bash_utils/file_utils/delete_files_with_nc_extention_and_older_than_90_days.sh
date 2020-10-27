20efd497fd885fd20b790416c5485f05cd1ec3c5
shadhini <jshadhiniaseka@gmail.com>
2020-10-27 15:36:00 +0530

    delete files using find command

--- bash_utils/file_utils/delete_files_with_nc_extention_and_older_than_90_days.sh
+++ bash_utils/file_utils/delete_files_with_nc_extention_and_older_than_90_days.sh
@@ -1,9 +1,1 @@
-files=$(find /mnt/disks/wrf_nfs/ -name "*.nc" -type f -mtime +90)
-
-for file in $files;
-do
-    echo "Deleting..."
-    echo $file
-    rm -v $file
-    echo "Deleted"
-done
+find /mnt/disks/wrf_nfs/ -name "*.nc" -type f -mtime +90 -exec rm -f {} \;
